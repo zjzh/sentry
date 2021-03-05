56055e7fdcc7e730b11c34889b8312b7dcd5a3b4
Stephen Cefali <scefali@sentry.io>
2021-03-05 12:48:45 -0800

    checkin

--- src/sentry/demo/demo_manager.py
+++ src/sentry/demo/demo_manager.py
@@ -1,14 +1,11 @@
 from django.core.cache import cache
-
 from django.conf import settings
+from typing import List, Tuple
+
+
 from sentry.models import (
     User,
     Organization,
-    OrganizationStatus,
-    OrganizationMember,
-    OrganizationMemberTeam,
-    Project,
-    ProjectKey,
 )
 
 from .tasks import create_demo_org_user
@@ -19,6 +16,7 @@ BUFFER_SIZE = 3
 
 class DemoManager:
     def populate_buffer(self):
+        print("curr buffer size", self.current_buffer_size)
         num_to_populate = BUFFER_SIZE - self.current_buffer_size
         if num_to_populate < 1:
             return
@@ -31,10 +29,10 @@ class DemoManager:
         return "demo:orgs_users"
 
     @property
-    def get_current_buffer(self):
+    def get_current_buffer(self) -> List[Tuple[User, Organization]]:
         return cache.get(self.cache_key) or []
 
-    def add_user_org(self, user: User, org: Organization):
+    def add_user_org(self, user: User, org: Organization) -> None:
         cached_demo_user_orgs = self.get_current_buffer
         cached_demo_user_orgs.append((user.id, org.id))
         cache.set(self.cache_key, cached_demo_user_orgs)
@@ -55,7 +53,7 @@ class DemoManager:
         return (user, org)
 
     @property
-    def current_buffer_size(self):
+    def current_buffer_size(self) -> int:
         return len(self.get_current_buffer)
 
 
--- src/sentry/web/frontend/demo_start.py
+++ src/sentry/web/frontend/demo_start.py
@@ -1,18 +1,8 @@
-import petname
-
 from django.http import Http404
 from django.conf import settings
 from django.db import transaction
 
 from sentry.demo.demo_manager import get_user_org
-from sentry.models import (
-    User,
-    Organization,
-    OrganizationMember,
-    OrganizationMemberTeam,
-    Project,
-    ProjectKey,
-)
 from sentry.utils import auth
 from sentry.web.frontend.base import BaseView
 
@@ -27,6 +17,7 @@ class DemoStartView(BaseView):
         if not settings.DEMO_MODE:
             raise Http404
 
+        # TODO: handle case where no user/org is rready
         (user, org) = get_user_org()
 
         auth.login(request, user)
