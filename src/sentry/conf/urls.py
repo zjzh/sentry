d99691b75090ee443f84da79b4ff6c8a37e7f7d4
Dan Fuller <dfuller@sentry.io>
2021-04-19 16:17:15 -0700

    hacks to fix error with 500 view

--- src/sentry/conf/urls.py
+++ src/sentry/conf/urls.py
@@ -3,14 +3,13 @@ from django.conf.urls import url
 
 from sentry.web.frontend.csrf_failure import CsrfFailureView
 from sentry.web.frontend.error_404 import Error404View
-from sentry.web.frontend.error_500 import Error500View
+from sentry.web.frontend.error_500 import error_500_view
 from sentry.web.urls import urlpatterns as web_urlpatterns
 
 handler404 = Error404View.as_view()
-handler500 = Error500View.as_view()
 
 urlpatterns = [
-    url(r"^500/", handler500, name="error-500"),
+    url(r"^500/", error_500_view, name="error-500"),
     url(r"^404/", handler404, name="error-404"),
     url(r"^403-csrf-failure/", CsrfFailureView.as_view(), name="error-403-csrf-failure"),
 ]
