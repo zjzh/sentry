0111d9672df426187808157c593c41c95a218559
Nisanthan Nanthakumar <nisanthan.nanthakumar@sentry.io>
2021-08-09 16:54:02 -0700

    docker compose file watching

---
+++ integrations/gitea/README.md
@@ -0,0 +1,9 @@
+# Run in Docker
+docker-compose up
+# use -d flag to run in background
+
+# Tear down
+docker-compose down
+
+# To re-build
+docker-compose build
--- integrations/gitea/index.js
+++ integrations/gitea/index.js
@@ -6,5 +6,8 @@ const PORT = process.env.PORT || 7500;
 
 // const mongoURI = 'mongodb://root:password@localhost:27017/db';
 
+app.get('/', async (req, res) => {
+  res.send('HELLO WORLD');
+});
 // Start express on the defined port
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
