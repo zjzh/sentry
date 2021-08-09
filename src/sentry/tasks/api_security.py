9451d04233545877713bdd69ea46be7298e00d2f
Aniket Das <aniket.das@sentry.io>
2021-08-09 15:32:01 -0700

    Now able to read snuba to find transactions in the last X seconds

--- src/sentry/tasks/api_security.py
+++ src/sentry/tasks/api_security.py
@@ -1,8 +1,22 @@
 import logging
+from datetime import datetime, timedelta
 
+from sentry.snuba.discover import query
 from sentry.tasks.base import instrumented_task, retry
 
 logger = logging.getLogger("sentry.tasks.api_security")
+
+
+class SecurityException(BaseException):
+    pass
+
+
+def _is_malicious_ip(ip_address):
+    return ip_address == "2601:642:4002:ba70:538:a4b:5a27:921e"
+
+
+def _create_security_issue():
+    logger.error("Received a request from a malicious IP Address")
 
 
 @instrumented_task(
@@ -11,8 +25,22 @@ logger = logging.getLogger("sentry.tasks.api_security")
     default_retry_delay=60 * 5,
     max_retries=2,
 )
-@retry()
+@retry(exclude=SecurityException)
 def test_task():
-    logger.info("=" * 80)
-    logger.info("The task worked!!!")
-    logger.info("=" * 80)
+    """Runs every 5 minutes"""
+    now = datetime.now()
+    results = query(
+        selected_columns=["user.ip", "count()"],
+        query="has:user.ip",
+        params={
+            "organization_id": 1,
+            "project_id": [1],
+            "start": now - timedelta(seconds=20),
+            "end": now,
+        },
+        orderby="-count()",
+    )
+    logger.info(results)
+    for query_result in results["data"]:
+        if _is_malicious_ip(query_result["user.ip"]):
+            _create_security_issue()
